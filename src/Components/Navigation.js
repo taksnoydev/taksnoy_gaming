766c600c719c4a835a41689efd8498840f1880bb
teddy900224 <teddy900224@gmail.com>
2020-05-16 19:39:39 +0800

    changed href

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -56,7 +56,7 @@ export class Navigation extends Component {
                         <p>HOME</p> 
                     </a>
                     
-                    <a href="/contact" className="Navigation__link"> 
+                    <a href="/taksnoy_brawlstars/contact" className="Navigation__link"> 
                         <p >CONTACT</p>
                     </a>
 
